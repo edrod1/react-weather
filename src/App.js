5187d1990ec532d7f33400fef6fcb222a20d1ddc
Edgar <edgarx28@outlook.com>
2022-10-20 18:39:54 -0400

    Finished app styling

--- src/App.js
+++ src/App.js
@@ -1,35 +1,75 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 function App() {
+  const [weatherData, setWeatherData] = useState({})
+  const [location, setLocation] = useState("")
+
+
+  const API_KEY = "7c8004625e900052ab9f80d73d92abe1"
+  const url = (`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`)
+
+  const getData = (e) => {
+    if (e.key === "Enter") {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => setWeatherData(data))
+      console.log(weatherData)
+      setLocation('')
+    }
+
+  }
+
+  // useEffect(() => {
+  //   getData()
+  // }, [])
+
 
   return (
     <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          placeholder="Enter Location"
+          type="text"
+          onChange={e => setLocation(e.target.value)}
+          onKeyPress={getData}
+        />
+      </div>
+
+
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dallas</p>
+            <p>{weatherData.name}</p>
           </div>
           <div className="temp">
-            <h1>60ºF</h1>
+            {weatherData.weather ? <h1>{weatherData.main.temp.toFixed()}ºF</h1> : null}
+
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {weatherData.weather ? <p>{weatherData.weather[0].main}</p> : null}
           </div>
         </div>
-        <div className="bottom">
-          <div className="feels">
-            <p clasName="bold">65ºF</p>
-            <p >Feels Like</p>
+
+        {weatherData.name &&
+          <div className="bottom">
+            <div className="feels">
+              {weatherData.main ? <p className="bold">{weatherData.main.feels_like.toFixed()}ºF</p> : null}
+              <p >Feels Like</p>
+            </div>
+            <div className="humidity">
+              {weatherData.main ? <p className="bold">{weatherData.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {weatherData.wind ? <p className="bold">{weatherData.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humidity">
-            <p clasName="bold">20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p clasName="bold">12 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        }
+
+
+
       </div>
     </div>
   );
